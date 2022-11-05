a5d0678d50baca8e08d5159cc675a577cedc7def
ddiaz-at-137082106675 <ddiaz@parameta.co>
2022-11-05 09:49:06 -0500

    service-gateway

--- service-libros/src/main/java/com/jesusta/biblioteca/libros/ServiceLibrosApplication.java
+++ service-libros/src/main/java/com/jesusta/biblioteca/libros/ServiceLibrosApplication.java
@@ -6,9 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
-@EnableEurekaClient
 @SpringBootApplication
+@EnableEurekaClient
 @EnableFeignClients
 @EnableHystrix
 @EnableHystrixDashboard
